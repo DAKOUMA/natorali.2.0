95704a845ed07f742d3b7f3191e95b493d44975b
DAKOUMA <ramerison.anselme@gmail.com>
2024-09-10 00:29:47 +0300

    10/09/2024

--- src/pages/about/Content.js
+++ src/pages/about/Content.js
@@ -1,22 +1,22 @@
-import { huile, force } from "../../assets/images/images"
+import { huile, force, sesame, tamanu } from "../../assets/images/images"
 
 const contentText = [
     {
         name: 25,
-        title: 'title one',
-        text: 'TEXT 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
-        image: huile
+        title: 'Création',
+        text: "En 2014, l'huilerie Natorali a vu le jour avec la production de son tout premier produit : l'huile de Foraha, une huile précieuse extraite du Calophyllum inophyllum, réputée pour ses propriétés curatives et régénératrices. Ce lancement marquait le début de l'engagement de Natorali à offrir des produits naturels et de qualité.",
+        image: tamanu
     },
     {
         name: 50,
-        title: 'title 2',
-        text: 'TEXT 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
-        image: huile
+        title: 'Sésame et Ricin',
+        text: "Trois ans après son lancement initial, en 2017, Natorali élargit sa gamme avec deux nouveaux produits phares : l'huile de Sésame et l'huile de Ricin. L'huile de Sésame, connue pour ses vertus nourrissantes et protectrices pour la peau et les cheveux, et l'huile de Ricin, prisée pour ses bienfaits fortifiants et stimulants pour la croissance des cheveux, viennent enrichir l'offre de l'huilerie.",
+        image: sesame
     },
     {
         name: 100,
-        title: 'title 3',
-        text: 'TEXT 3 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+        title: 'Vision internationale',
+        text: "En 2022, après avoir solidement établi sa présence locale, l'huilerie Natorali aspire à se projeter sur la scène internationale. Avec une gamme de produits de qualité et un savoir-faire artisanal reconnu, l'entreprise ambitionne de faire découvrir ses huiles naturelles à un public mondial et de développer ses activités à l'échelle internationale.",
         image: huile
     },
 ]
--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -3,6 +3,7 @@ import NavBar from '../../components/navbar/NavBar'
 import Footer from '../../components/footer/Footer'
 import productList from './productList'
 import ProductCard from './ProductCard'
+import { motion } from 'framer-motion'
 
 const Product = () => {
   useEffect(() => {
@@ -11,9 +12,26 @@ const Product = () => {
   return (
     <>
       <NavBar />
+      <div className='drop'>
+      </div>
+      <div className='drop1' >
+      </div>
+      <div className='drop2' >
+      </div>
+      <div className='drop4' >
+      </div>
       <main className="product-container container">
-        <h1>Nos Produits</h1>
-        <h2>Découvrez le meilleur de Madagascar en bouteille avec nos produits</h2>
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.2, duration: 2}}
+        >Nos Produits</motion.h1>
+        <motion.h2
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.4, duration: 2}}
+        >
+          Découvrez le meilleur de Madagascar en bouteille avec nos produits</motion.h2>
         <article className='productCard-container'>
           {
             productList.map((value, index) => (
